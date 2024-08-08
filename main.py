df0fc68da4b277aa0c311f139c404c777e43bc8f
jerin0007 <jerinjacob77777@gmail.com>
2024-08-08 15:56:51 +0530

    color_pallet

--- main.py
+++ main.py
@@ -1,13 +1,60 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, request
-from faster_whisper import WhisperModel
-import base64
-import os
 
-# os.environ[''] = 'True'
+from scipy import cluster
+import pandas as pd
+import math
+import requests
+import cv2
+import matplotlib.pyplot as plt
 
-model = WhisperModel("base.en", device="cpu", compute_type="int8")
+
+def rgb2hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+def get_color_pallete(input_file, num_colors):
+    img = plt.imread(input_file)
+
+    red, green, blue = [], [], []
+    for line in img:
+        for pixel in line:
+            r, g, b = pixel
+            red.append(r)
+            green.append(g)
+            blue.append(b)
+
+    df = pd.DataFrame({
+        'red': red,
+        'green': green,
+        'blue': blue
+    })
+
+    df['standardized_red'] = cluster.vq.whiten(df['red'])
+    df['standardized_green'] = cluster.vq.whiten(df['green'])
+    df['standardized_blue'] = cluster.vq.whiten(df['blue'])
+
+    color_pallete, distortion = cluster.vq.kmeans(df[['standardized_red', 'standardized_green', 'standardized_blue']],
+                                                num_colors)
+    colors = []
+    red_std, green_std, blue_std = df[['red', 'green', 'blue']].std()
+    for color in color_pallete:
+        scaled_red, scaled_green, scaled_blue = color
+        colors.append((
+            math.ceil(scaled_red * red_std),
+            math.ceil(scaled_green * green_std),
+            math.ceil(scaled_blue * blue_std)
+        ))
+
+
+    final_colors = []
+
+    for color in colors:
+        final_colors.append(rgb2hex(color[0], color[1], color[2]))
+
+    return final_colors
+
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -21,22 +68,37 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route("/transcribe", methods=["POST"])
-def transcribe():
-    # encode_string = base64.b64encode(open("audio.wav", "rb").read())
-    wav_file = open("./test.m4a", "wb")
-    decode_string = base64.b64decode(request.json["file"])
-    wav_file.write(decode_string)
-    print("saved")
+@app.route("/get_pallet", methods=["POST"])
+def get_pallet():
 
-    segments, info = model.transcribe("test.m4a", beam_size=5, language="en", vad_filter=True)
-    print("crossed")
-    full_text = ""
+    url = "https://api.freepik.com/v1/resources?term=aloe vera&page=1&limit=1&filters[content_type][photo]=1"
 
-    for segment in segments:
-        full_text += segment.text + " "
-        
-    print(full_text)
+    headers = {
+        "Accept-Language": "<accept-language>",
+        "x-freepik-api-key": "FPSXd4370660d2f441ea9db1508cfae125e7"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    print(response.json()["data"][0]["image"]["source"]["url"])
+
+    bytes = requests.get(response.json()["data"][0]["image"]["source"]["url"]).content
+
+    f = open("img.jpg", "wb")
+    f.write(bytes)
+    f.close()
+
+    image = cv2.imread('img.jpg')
+    h, w = image.shape[:2]
+    aspect_ratio = w / h
+
+    resized_image = cv2.resize(image, (200, int(200 / aspect_ratio)))
+
+    img = cv2.imwrite("img.jpg", resized_image)
+
+    colors_list = get_color_pallete("./img.jpg", 10)
+
+    print(colors_list)
+
 
 
     return full_text
